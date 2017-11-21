46aa4130f17ca1a1be4c52b8f8f6581eb9a70f77
Piotr Ławniczak <thestuntman@outlook.com>
2017-11-21 20:53:00 +0100

    added JavaFX interface schema

---
+++ ECGSim/src/ui/MainView.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
